643ad5b2ce6178689c05d12e06218a4c98f1f868
hristovt <46815781+hristovt@users.noreply.github.com>
2020-04-21 22:49:55 +0200

    Update omega_J.m

--- simulation/matlab/omega_J.m
+++ simulation/matlab/omega_J.m
@@ -1,7 +1,10 @@
 function x = omega_J(p, q, r)
+
     J = evalin('base','J');
     x = [0; 0; 0];
+    
     x(1) = (J(3) - J(2))*q*r;
     x(2) = (J(1) - J(3))*r*p;
     x(3) = (J(2) - J(1))*p*q;
+    
 end
