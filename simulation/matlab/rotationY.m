ab9182fe6010226e85b855cf79640b5e83db2d6a
hristovt <46815781+hristovt@users.noreply.github.com>
2020-04-21 22:46:44 +0200

    Update rotationY.m

--- simulation/matlab/rotationY.m
+++ simulation/matlab/rotationY.m
@@ -1,8 +1,9 @@
-function [R] = rotationY(angle)
 %Rotation around Y axis; cosd angle in degrees
+function [R] = rotationY(angle)
 
-R=[cosd(angle), 0,-sind(angle);
-       0      , 1,     0      ;
+R=[cosd(angle), 0,-sind(angle) ;
+       0      , 1,     0       ;
    sind(angle), 0, cosd(angle)];
+   
 end
 
