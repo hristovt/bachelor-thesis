9d2d8413cb7ebfac940bb68701a31408f02b18f8
hristovt <46815781+hristovt@users.noreply.github.com>
2020-04-21 22:46:03 +0200

    Update rotationX.m

--- simulation/matlab/rotationX.m
+++ simulation/matlab/rotationX.m
@@ -1,8 +1,8 @@
 %Rotation around X axis; cosd angle in degrees
 function [R] = rotationX(angle)
 
-R=[1,     0      ,     0      ;
-   0, cosd(angle), sind(angle);
-   0,-sind(angle),cosd(angle)];
+R=[1,     0      ,     0       ;
+   0, cosd(angle), sind(angle) ;
+   0,-sind(angle), cosd(angle)];
 end
 
