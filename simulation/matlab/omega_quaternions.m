5c8b830e1a15d992f824d2d637b73ffb8f276429
hristovt <46815781+hristovt@users.noreply.github.com>
2020-04-21 22:52:11 +0200

    Update omega_quaternions.m

--- simulation/matlab/omega_quaternions.m
+++ simulation/matlab/omega_quaternions.m
@@ -1,9 +1,9 @@
+%Angular velocity with quaternions
 function X = omega_quaternions(p,q,r)
 
-    X=[0 -p -q -r;
-       p  0  r -q;
-       q -r  0  p;
+    X=[0 -p -q -r ;
+       p  0  r -q ;
+       q -r  0  p ;
        r  q -p  0];
 
 end
-
