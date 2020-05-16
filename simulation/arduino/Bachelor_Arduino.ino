fba1c55b2fa07079a04d79882a9e3a98f015df3e
hristovt <46815781+hristovt@users.noreply.github.com>
2020-05-16 09:49:37 +0200

    Update Bachelor_Arduino.ino

--- simulation/arduino/Bachelor_Arduino.ino
+++ simulation/arduino/Bachelor_Arduino.ino
@@ -1,60 +1,193 @@
 #include "MPU9250.h"
+#include "MadgwickAHRS.h"
+#include "Servo.h"
+#include "PID_v1.h"
+#include "math.h"
+#define PIN_SERVO_P 9
+#define PIN_SERVO_Y 10
 
-// an MPU9250 object with the MPU-9250 sensor on SPI bus 0 and chip select pin 10
-MPU9250 IMU(Wire,0x68);
-int status;
-float acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z;
-
-// Create a union to easily convert float to byte
 typedef union{
   float number;
   uint8_t bytes[4];
 } FLOATUNION_t;
 
-// Create the variable you want to send
-FLOATUNION_t a_x;
-FLOATUNION_t a_y;
-FLOATUNION_t a_z;
+FLOATUNION_t delta_p;
+FLOATUNION_t delta_y;
 
+MPU9250 IMU(Wire, 0x68); //I2C conntection with the IMU
+int status;
+
+float ax, ay, az, gx, gy, gz, mx, my, mz; //Sensor accelerometar, gyro and magnetometar x,y and z components
+float ag = 0.101972;                      //Transformation from m/s^2 to G's
+float pitch, yaw, roll;
+
+Madgwick filter;
+unsigned long microsPerReading, microsPrevious;
+
+double Setpoint_p, Input_p, Output_p;
+float Kp_p=1;
+float Kd_p=1.5;
+float Ki_p=0;
+PID tvc_p(&Input_p, &Output_p, &Setpoint_p, Kp_p, Ki_p, Kd_p, DIRECT);
+
+double Setpoint_y, Input_y, Output_y;
+float Kp_y=1;
+float Kd_y=1.5;
+float Ki_y=0;
+PID tvc_y(&Input_y, &Output_y, &Setpoint_y, Kp_y, Ki_y, Kd_y, DIRECT);
+
+
+unsigned long time;
+
+Servo servo_p;
+Servo servo_y;
 
 void setup() {
-  // serial to display data
   Serial.begin(115200);
+  while (!Serial) {}
+
   // start communication with IMU 
   status = IMU.begin();
+  if (status < 0) {
+    Serial.println("IMU initialization unsuccessful");
+    Serial.println("Check IMU wiring or try cycling power");
+    Serial.print("Status: ");
+    Serial.println(status);
+    while(1) {}
+  }
 
+  status = IMU.begin();                                 
+  IMU.setAccelRange(MPU9250::ACCEL_RANGE_2G);
+  IMU.setGyroRange(MPU9250::GYRO_RANGE_250DPS);
+  IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_5HZ);
+
+  IMU.calibrateAccel();
+  IMU.calibrateGyro();
+  //IMU.calibrateMag();
+  
+  filter.begin(25);
+  microsPerReading = 1000000 / 25;
+  microsPrevious = micros();
+
+  Input_p=pitch;
+  Setpoint_p=0;
+  tvc_p.SetMode(AUTOMATIC);
+  tvc_p.SetSampleTime(250);
+  //tvc_p.SetOutputLimits(0, 10);
+  servo_p.attach(PIN_SERVO_P);
+  servo_p.write(90);
+
+  Input_y=yaw;
+  Setpoint_y=0;
+  tvc_y.SetMode(AUTOMATIC);
+  tvc_y.SetSampleTime(250);
+  //tvc_y.SetOutputLimits(-10, 10);
+  servo_y.attach(PIN_SERVO_Y);
+  servo_y.write(90);
 }
 
 void loop() {
-  IMU.readSensor();
-  
-  acc_x=IMU.getAccelX_mss();
-  acc_y=IMU.getAccelY_mss();
-  acc_z=IMU.getAccelZ_mss();
+  unsigned long microsNow;
+  microsNow = micros();
+  time=millis();
+  if (microsNow - microsPrevious >= microsPerReading) {
 
-  gyro_x=IMU.getGyroX_rads();
-  gyro_y=IMU.getGyroY_rads();
-  gyro_z=IMU.getGyroZ_rads();
+    IMU.readSensor();
 
-  a_x.number = acc_x;
-  a_y.number = acc_y;
-  a_z.number = acc_z;
+    //M/s^2 to G's
+    ax = IMU.getAccelX_mss() * ag;
+    ay = IMU.getAccelY_mss() * ag;
+    az = IMU.getAccelZ_mss() * ag;
 
-  Serial.write('A'); 
+    //Rad/s to deg/s
+    gx = IMU.getGyroX_rads() * (180 / PI);
+    gy = IMU.getGyroY_rads() * (180 / PI);
+    gz = IMU.getGyroZ_rads() * (180 / PI);
 
-  for (int i=0; i<4; i++){
-   Serial.write(a_x.bytes[i]);
+
+    filter.updateIMU(gx, gy, gz, ax, ay, az);
+
+    
+    pitch = filter.getPitch();
+    
+    
+    yaw = filter.getRoll();
+    
+    
+    if(yaw>=-180 && yaw<=0){
+      yaw=(yaw+180.0f)*-1;
+    }
+    
+    if(yaw>0 && yaw<=180){
+      yaw=(yaw-180.0f)*-1;
+    }
+
+    Serial.print("Pitch: ");
+    Serial.print(pitch, 2);
+    Serial.print("\t");
+
+    Serial.print("Yaw: ");
+    Serial.print(yaw, 2);
+    Serial.print("\t");
+
+
+
+    if(time>40000){
+      if(pitch<0){
+        tvc_p.SetControllerDirection(DIRECT);
+        Input_p=round(pitch);
+        tvc_p.Compute();
+        servo_p.write(90+Output_p*-1);
+        delta_p.number=Output_p*-1;
+      }
+
+      if(pitch>0){
+        tvc_p.SetControllerDirection(REVERSE);
+        Input_p=round(pitch);
+        tvc_p.Compute();
+        servo_p.write(90+Output_p);
+        delta_p.number=Output_p;
+      }
+
+      if(yaw<0){
+        tvc_y.SetControllerDirection(DIRECT);
+        Input_y=round(yaw);
+        tvc_y.Compute();        
+        servo_y.write(90+Output_y);
+        delta_y.number=Output_y;
+      }
+
+       if(yaw>0){
+        tvc_y.SetControllerDirection(REVERSE);
+        Input_y=round(yaw);
+        tvc_y.Compute();
+        servo_y.write(90+Output_y*-1);
+        delta_y.number=Output_y*-1;
+      }
+
+      Serial.print("Output_y: ");
+      Serial.print(Output_y);
+      Serial.print("\t");
+
+      Serial.print("Output_p: ");
+      Serial.print(Output_p);
+      Serial.print("\n");
+
+        //Serial.write('A');
+
+        //for (int i=0; i<4; i++){
+        //   Serial.write(delta_p.bytes[i]);
+        //}
+
+        //for (int i=0; i<4; i++){
+        //   Serial.write(delta_y.bytes[i]);
+        //}
+        
+        //Serial.print("\n");
+    }
+
+    microsPrevious = microsPrevious + microsPerReading;
   }
 
-  for (int i=0; i<4; i++){
-   Serial.write(a_y.bytes[i]);
-  }
-
-  for (int i=0; i<4; i++){
-   Serial.write(a_z.bytes[i]);
-  }
-      
-  Serial.print('\n');
-  
   delay(50);
 }
