aac0a3022ecd5acd586f10f4b3535c892ed543ad
hristovt <46815781+hristovt@users.noreply.github.com>
2020-05-16 10:15:15 +0200

    Update Bachelor_Arduino.ino

--- simulation/arduino/Bachelor_Arduino.ino
+++ simulation/arduino/Bachelor_Arduino.ino
@@ -1,88 +1,87 @@
 #include "MPU9250.h"
 #include "MadgwickAHRS.h"
-#include "Servo.h"
-#include "PID_v1.h"
+#include "Servo.h"          //Including the libraries that wil be needed for the code.
+#include "PID_v1.h"         //The ones that are harder to find will be on the repository.
 #include "math.h"
-#define PIN_SERVO_P 9
+//---------------------------------------------------------//
+#define PIN_SERVO_P 9        //Defining the pins for the servos
 #define PIN_SERVO_Y 10
-
+//---------------------------------------------------------//
 typedef union{
   float number;
   uint8_t bytes[4];
-} FLOATUNION_t;
-
+} FLOATUNION_t;                 //Converting the TVC angles into bits to transfer them, through Serial connection, 
+                                //to the Simulink Simulation
 FLOATUNION_t delta_p;
 FLOATUNION_t delta_y;
-
-MPU9250 IMU(Wire, 0x68); //I2C conntection with the IMU
+//--------------------------------------------------------//
+MPU9250 IMU(Wire, 0x68);                                  //I2C conntection with the IMU
 int status;
-
-float ax, ay, az, gx, gy, gz, mx, my, mz; //Sensor accelerometar, gyro and magnetometar x,y and z components
-float ag = 0.101972;                      //Transformation from m/s^2 to G's
+//--------------------------------------------------------//
+float ax, ay, az, gx, gy, gz;                             //Sensor accelerometar and gyro  x,y and z components
+float ag = 0.101972;                                      //Transformation from m/s^2 to G's
 float pitch, yaw, roll;
-
-Madgwick filter;
-unsigned long microsPerReading, microsPrevious;
-
+//--------------------------------------------------------//
+Madgwick filter;                                          //Creating the filter object
+unsigned long microsPerReading, microsPrevious;           //Variables for keeping track of the filter
+//--------------------------------------------------------//
 double Setpoint_p, Input_p, Output_p;
-float Kp_p=1;
+float Kp_p=1;                                             //PID controller for the pitch angle motor
 float Kd_p=1.5;
 float Ki_p=0;
 PID tvc_p(&Input_p, &Output_p, &Setpoint_p, Kp_p, Ki_p, Kd_p, DIRECT);
-
+//--------------------------------------------------------//
 double Setpoint_y, Input_y, Output_y;
-float Kp_y=1;
+float Kp_y=1;                                             //PID controller for the yaw angle motor
 float Kd_y=1.5;
 float Ki_y=0;
 PID tvc_y(&Input_y, &Output_y, &Setpoint_y, Kp_y, Ki_y, Kd_y, DIRECT);
-
-
+//--------------------------------------------------------//
 unsigned long time;
 
-Servo servo_p;
+Servo servo_p;                                            //Declaring the servo motors, pitch and yaw
 Servo servo_y;
-
+//--------------------------------------------------------//
 void setup() {
   Serial.begin(115200);
   while (!Serial) {}
-
-  // start communication with IMU 
-  status = IMU.begin();
+                                                 
+  status = IMU.begin();                                   // Start communication with IMU 
   if (status < 0) {
     Serial.println("IMU initialization unsuccessful");
     Serial.println("Check IMU wiring or try cycling power");
-    Serial.print("Status: ");
+    Serial.print("Status: ");                             //If the communication with the IMU is unsuccessful, it will display a message
     Serial.println(status);
     while(1) {}
   }
-
-  status = IMU.begin();                                 
+                                
   IMU.setAccelRange(MPU9250::ACCEL_RANGE_2G);
-  IMU.setGyroRange(MPU9250::GYRO_RANGE_250DPS);
+  IMU.setGyroRange(MPU9250::GYRO_RANGE_250DPS);           //Setting the IMU settings
   IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_5HZ);
 
-  IMU.calibrateAccel();
+  IMU.calibrateAccel();                                   //Callibrating the IMU
   IMU.calibrateGyro();
-  //IMU.calibrateMag();
   
-  filter.begin(25);
+  filter.begin(25);                                       
   microsPerReading = 1000000 / 25;
   microsPrevious = micros();
 
-  Input_p=pitch;
+  Input_p=pitch;                                          //Setting the PID controller for the pitch angle servo
   Setpoint_p=0;
   tvc_p.SetMode(AUTOMATIC);
   tvc_p.SetSampleTime(250);
   //tvc_p.SetOutputLimits(0, 10);
-  servo_p.attach(PIN_SERVO_P);
-  servo_p.write(90);
 
-  Input_y=yaw;
+  Input_y=yaw;                                            //Setting the PID controller for the yaw angle servo
   Setpoint_y=0;
   tvc_y.SetMode(AUTOMATIC);
   tvc_y.SetSampleTime(250);
   //tvc_y.SetOutputLimits(-10, 10);
-  servo_y.attach(PIN_SERVO_Y);
+    
+  servo_p.attach(PIN_SERVO_P);                            //Setting the servo for the pitch angle
+  servo_p.write(90);
+  
+  servo_y.attach(PIN_SERVO_Y);                            //Setting the servo for the yaw angle
   servo_y.write(90);
 }
 
@@ -90,31 +89,25 @@ void loop() {
   unsigned long microsNow;
   microsNow = micros();
   time=millis();
+  
   if (microsNow - microsPrevious >= microsPerReading) {
 
-    IMU.readSensor();
-
-    //M/s^2 to G's
-    ax = IMU.getAccelX_mss() * ag;
+    IMU.readSensor();                                    //Read the IMU values
+    
+    ax = IMU.getAccelX_mss() * ag;                       //Converting from m/s^2 to G's
     ay = IMU.getAccelY_mss() * ag;
     az = IMU.getAccelZ_mss() * ag;
 
-    //Rad/s to deg/s
-    gx = IMU.getGyroX_rads() * (180 / PI);
+    gx = IMU.getGyroX_rads() * (180 / PI);               //Converting from rad/s to deg/s
     gy = IMU.getGyroY_rads() * (180 / PI);
     gz = IMU.getGyroZ_rads() * (180 / PI);
 
+    filter.updateIMU(gx, gy, gz, ax, ay, az);            //Giving the filter the acc and gyro values
 
-    filter.updateIMU(gx, gy, gz, ax, ay, az);
-
+    pitch = filter.getPitch();                           //Getting the pitch and yaw angles
+    yaw = filter.getRoll();                              //The yaw is equal to the roll is because of the possition of the IMU
     
-    pitch = filter.getPitch();
-    
-    
-    yaw = filter.getRoll();
-    
-    
-    if(yaw>=-180 && yaw<=0){
+    if(yaw>=-180 && yaw<=0){                             //Converting the yaw angle to start from 0 degrees and to go + or - from there
       yaw=(yaw+180.0f)*-1;
     }
     
@@ -122,7 +115,7 @@ void loop() {
       yaw=(yaw-180.0f)*-1;
     }
 
-    if(time>40000){
+    if(time>40000){                                     //Waiting 40 seconds for the IMU to callibrate and then to start to calcualte with the PID
       if(pitch<0){
         tvc_p.SetControllerDirection(DIRECT);
         Input_p=round(pitch);
@@ -154,18 +147,19 @@ void loop() {
         servo_y.write(90+Output_y*-1);
         delta_y.number=Output_y*-1;
       }
-
+//--------------------------------------------------------//
         Serial.write('A');
 
         for (int i=0; i<4; i++){
            Serial.write(delta_p.bytes[i]);
-        }
-
+        } 
+                                                          //Sending the PID values to to the Simulink simulation
         for (int i=0; i<4; i++){
            Serial.write(delta_y.bytes[i]);
         }
         
         Serial.print("\n");
+//--------------------------------------------------------//      
     }
 
     microsPrevious = microsPrevious + microsPerReading;
